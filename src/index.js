c577bec7ef4b2ca0670f2a72f052b977ddaa98e1
TURKCELL\EXT02D47957 <mucahit.sahin@amaris.com>
2022-01-02 20:09:35 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/home/App';
 import reportWebVitals from './reportWebVitals';
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                {/* <Route path="expenses" element={<Expenses/>}/> */}
+            </Routes>
+        </BrowserRouter>,
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
