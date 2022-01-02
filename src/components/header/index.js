e4ff3a99d23097323b11894a1798fe3637efd8f7
ahmet yıldız <ahmet@epsilam.com>
2022-01-02 22:51:05 +0300

    header component created

---
+++ src/components/header/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Header.scss'
+import {FaAngleRight} from 'react-icons/fa';
+
+
+const Header = () => {
+    return <nav className="navbar navbar-expand-lg header-main">
+        <div className="container-fluid header-in-container container">
+            <div>
+                <a className="navbar-brand navbar-title" href="/">Ahmet YILDIZ</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon">X</span>
+                </button>
+            </div>
+            <div className="collapse navbar-collapse navbar-item-container" id="navbarNav">
+                <ul className=" navbar-list">
+                    <li className="nav-item navbar-list-item">
+                        <a className="nav-link navbar-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item navbar-list-item">
+                        <a className="nav-link navbar-link" href="#">Features</a>
+                    </li>
+                    <li className="nav-item navbar-list-item">
+                        <a className="nav-link navbar-link" href="#">Pricing</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className={"work-request-area"}>
+                <a className={"work-request-link"} href="">Work Request</a> <span
+                className={"right-arrow"}><FaAngleRight color={'#ffffff'}/></span>
+            </div>
+        </div>
+    </nav>
+
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/home/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+
 import {
     BrowserRouter,
     Routes,
@@ -17,7 +26,7 @@ ReactDOM.render(
                 <Route path="/" element={<App/>}/>
                 {/* <Route path="expenses" element={<Expenses/>}/> */}
             </Routes>
-        </BrowserRouter>,
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
